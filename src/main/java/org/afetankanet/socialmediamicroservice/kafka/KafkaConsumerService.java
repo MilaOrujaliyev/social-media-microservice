200110f8ea0f49b18093f56c4d0dff01f5d92031
Mila-U <88654285+Mila-U@users.noreply.github.com>
2023-12-22 01:13:59 +0300

    kafka listener implemented

---
+++ src/main/java/org/afetankanet/socialmediamicroservice/kafka/KafkaConsumerService.java
@@ -0,0 +1,19 @@
+package org.afetankanet.socialmediamicroservice.kafka;
+
+import org.afetankanet.socialmediamicroservice.model.Tweet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+
+    private final Logger logger = LoggerFactory.getLogger(KafkaConsumerService.class);
+
+    @KafkaListener(topics = "tweet_topic", groupId = "tweet_group_id")
+    public void consume(Tweet tweet) {
+        logger.info("Consumed message: " + tweet);
+        // Burada Tweet nesnesi ile ilgili işlemler yapılabilir.
+    }
+}
